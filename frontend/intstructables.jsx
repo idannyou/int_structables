87b2f9af112ba1d5087df375c0671c23e20b7834
Danny Ou <idannyou@gmail.com>
2017-03-27 10:47:36 -0400

    Changed window.store to store

--- frontend/intstructables.jsx
+++ frontend/intstructables.jsx
@@ -8,10 +8,10 @@ document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   let store, preloadedState;
   if (window.currentUser){
-    window.store = configureStore(preloadedState = {session: {currentUser: window.currentUser}});
+    store = configureStore(preloadedState = {session: {currentUser: window.currentUser}});
   } else {
-    window.store = configureStore();
+    store = configureStore();
   }
   Modal.setAppElement(document.body);
-  ReactDOM.render(<Root store={window.store} />, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
