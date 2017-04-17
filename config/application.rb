7954a545a19eb44ddaf24a03ed656bf9c62afa51
Danny Ou <idannyou@gmail.com>
2017-04-17 13:19:08 -0400

    Change protocol to https

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,8 @@ module IntStructables
         :access_key_id => ENV["s3_access_key_id"],
         :secret_access_key => ENV["s3_secret_access_key"],
         :s3_region => ENV["s3_region"],
-        :s3_protocol => :https
-      }
+      },
+      :s3_protocol => :https
     }
   end
 end
